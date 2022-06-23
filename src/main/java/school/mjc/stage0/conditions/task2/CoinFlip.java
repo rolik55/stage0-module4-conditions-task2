5c6a7de2fe0bdf008e2998558c1d9df67590648e
rolik55 <65602933+rolik55@users.noreply.github.com>
2022-06-23 13:49:56 +0300

    v3

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,7 +2,6 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-        from1UpTo1000--;
         if(from1UpTo1000 < 500)
             System.out.println("Eagle");
         else
